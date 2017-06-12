c70cfb8e138dff14948b47c3641d9e80d495e03e
Patrick Morgan <patrick.morgan@live.com.au>
2017-06-12 17:01:01 +1000

    Basic modals added and functional

---
+++ _js/modals.js
@@ -0,0 +1,37 @@
+// Get all the modals
+const modals = document.querySelectorAll('.modal');
+
+// Hide a modal
+const hideModal = (modal) => {
+    modal.classList.add("modal-hiding");
+
+    // Delay "display: none" to allow animation to finish
+    setTimeout(() => {
+        modal.classList.remove("modal-hiding");
+        modal.classList.add("modal-hidden");
+    }, 250);
+}
+
+// Setup event listeners
+modals.forEach((modal, i) => {
+    const btn = document.querySelector('#btn-' + modal.id);
+    const close = document.querySelector('#close-' + modal.id);
+
+    // Open modal
+    btn.onclick = (event) => {
+        event.preventDefault();
+        modal.classList.remove("modal-hidden");
+    };
+
+    // Close modal
+    close.onclick = () => {
+        hideModal(modal);
+    }
+
+    // Close modal by clicking outside it
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            hideModal(modal);
+        }
+    }
+});
